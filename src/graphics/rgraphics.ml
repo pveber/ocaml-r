fecf2afe1599389362188ae068527d68f70bd9e9
pveber <philippe.veber@gmail.com>
2012-07-03 15:04:16 +0200

    [Rgraphics] enriched hist interface

--- src/graphics/rgraphics.ml
+++ src/graphics/rgraphics.ml
@@ -6,9 +6,25 @@ module Stub = struct
 
 end
 
-let hist x = 
+let r_breaks = function
+| `n n -> Obj.magic (R.int n)
+| `l v -> Obj.magic v
+| `m `Sturges -> Obj.magic (R.string "Sturges")
+| `m `Scott -> Obj.magic (R.string "Scott")
+| `m `FD -> Obj.magic (R.string "FD")
+
+let hist ?breaks ?freq ?include_lowest ?right ?main ?xlab ?ylab ?xlim ?ylim x = 
   R.eval Stub.hist [
-    R.arg (fun x -> x) x ;
+    R.arg (fun x -> x)                  x ;
+    R.opt r_breaks     "breaks"         breaks ;
+    R.opt R.bool       "freq"           freq ;
+    R.opt R.bool       "include_lowest" include_lowest ;
+    R.opt R.bool       "right"          right ;
+    R.opt R.string     "main"           main;
+    R.opt R.string     "xlab"           xlab ;
+    R.opt R.string     "ylab"           ylab ;
+    R.opt R.float      "xlim"           xlim ;
+    R.opt R.float      "ylim"           ylim ;
   ]
 
 
--- src/graphics/rgraphics.mli
+++ src/graphics/rgraphics.mli
@@ -1,3 +1,10 @@
 (** Runtime R graphics library. *)
 
-val hist : float list R.t -> 'a R.t
+val hist : 
+  ?breaks:[`n of int | `l of float list R.t | `m of [`Sturges | `Scott | `FD]] ->
+  ?freq:bool ->
+  ?include_lowest:bool ->
+  ?right:bool ->
+  ?main:string -> ?xlab:string -> ?ylab:string ->
+  ?xlim:float -> ?ylim:float -> 
+  float list R.t -> 'a R.t
