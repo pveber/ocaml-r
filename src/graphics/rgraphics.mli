59d4eecc3520ba68823a07215864ee1cc7b3183f
pveber <philippe.veber@gmail.com>
2013-01-17 10:52:54 +0100

    graphics: better type of hist

--- src/graphics/rgraphics.mli
+++ src/graphics/rgraphics.mli
@@ -1,5 +1,11 @@
 (** Runtime R graphics library. *)
 
+type hist = < breaks : float list R.t ;
+              counts : float list R.t ;
+              density : float list R.t ;
+              mids : float list R.t ;
+              xname : string R.t ;
+              equidist : bool R.t >
 val hist : 
   ?breaks:[`n of int | `l of float list R.t | `m of [`Sturges | `Scott | `FD]] ->
   ?freq:bool ->
@@ -7,4 +13,23 @@ val hist :
   ?right:bool ->
   ?main:string -> ?xlab:string -> ?ylab:string ->
   ?xlim:float -> ?ylim:float -> 
-  float list R.t -> 'a R.t
+  float list R.t -> hist R.t
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
