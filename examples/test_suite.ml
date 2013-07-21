5340f86e5cf8964c16951a6751273b7d98195967
pveber <philippe.veber@gmail.com>
2013-07-21 17:11:38 +0200

    bugfix: undo non-interactive mode
    setting the embedded interpreter to non-interactive has one major
    consequence: signaling errors via [error] halts execution in a way
    that seems not compatible with [R_tryError]

--- examples/test_suite.ml
+++ examples/test_suite.ml
@@ -4,11 +4,14 @@ let _ =
   try
     ignore (R.eval_string "azer")
   with
-    R.Runtime_error (x,y) -> (
-      print_endline y ;
-      let r = Obj.repr y in
-      if Obj.is_block r then print_int (Obj.tag r)
-    )
+    R.Runtime_error (x,y) -> print_string y
+;;
+
+let _ =
+  try
+    ignore (R.eval_string "list()[[1]]")
+  with
+    R.Runtime_error (x,y) -> print_endline y
 ;;
 
 
