ef4a01e34223388a59bddb1b55d8cbe685bbd3fb
Philippe Veber <philippe.veber@gmail.com>
2014-09-17 05:22:39 +0200

    restaured stats

--- src/stats/r_stats.mli
+++ src/stats/r_stats.mli
@@ -7,42 +7,36 @@ val rnorm : ?mean:float -> ?sd:float -> int -> float list
     and [1.] respectively. *)
 
 
-(** {5 Testing} *)
+(** {5 Tests} *)
 
-(* class type fisher'test = object *)
-(*   inherit list_ *)
-(*   method p'value : float *)
-(*   method conf'int : (float * float) option *)
-(*   method estimate : float *)
-(*   (\* method null'value : real ; *\) *)
-(*   (\* method alternative : string ; *\) *)
-(*   (\* method method_ : string ; *\) *)
-(*   (\* method data'name : string *\) *)
-(* end *)
+val fisher'test :
+  ?alternative:[`two_sided | `greater | `less] ->
+  float list ->
+  float list ->
+  < p'value : float ;
+    conf'int : (float * float) option  ;
+    estimate : float ;
+    null'value : float ;
+    alternative : string ;
+    method_ : string ;
+    data'name : string >
 
-(* val fisher'test : *)
+val ks'test :
+  ?alternative:[`two_sided | `greater | `less] ->
+  float list -> float list ->
+  < statistic : float ;
+    p'value : float ;
+    alternative : string ;
+    method_ : string ;
+    data'name : string >
+
+val p'adjust :
+  ?method_ : [`holm | `hochberg | `hommel | `bonferroni | `BH | `BY | `fdr] ->
+  float list -> float list
+
+(* val fisher'test_2x2 : *)
 (*   ?alternative:[`two_sided | `greater | `less] -> *)
-(*   float list -> *)
-(*   float list -> *)
-(*   fisher'test *)
-
-(* (\* val fisher'test_2x2 : *\) *)
-(* (\*   ?alternative:[`two_sided | `greater | `less] -> *\) *)
-(* (\*   ff:int -> ft:int -> tf:int -> tt:int -> unit -> fisher'test listing R.t *\) *)
-
-
-(* val ks'test : *)
-(*   ?alternative:[`two_sided | `greater | `less] -> *)
-(*   float list -> float list -> *)
-(*   < statistic : float R.t ; *)
-(*     p'value : float R.t ; *)
-(*     alternative : string R.t ; *)
-(*     _method : string R.t ; *)
-(*     data'name : string R.t > listing R.t *)
-
-(* val p'adjust : *)
-(*   ?method_ : [`holm | `hochberg | `hommel | `bonferroni | `BH | `BY | `fdr] -> *)
-(*   float list -> float list *)
+(*   ff:int -> ft:int -> tf:int -> tt:int -> unit -> fisher'test listing R.t *)
 
 
 
@@ -54,19 +48,6 @@ val rnorm : ?mean:float -> ?sd:float -> int -> float list
 (*   val rnorm : ?mean:float R.t -> ?sd:float R.t -> int R.t -> float list R.t *)
 
 
-(*   (\** {5 Tests} *\) *)
-
-(*   val fisher'test : *)
-(*     ?alternative:string R.t -> *)
-(*     float list R.t -> *)
-(*     float list R.t -> *)
-(*     < p'value : float R.t ; *)
-(*       conf'int : float list R.t ; *)
-(*       estimate : float R.t ; *)
-(*       null'value : float R.t ; *)
-(*       alternative : string R.t ; *)
-(*       method_ : string R.t ; *)
-(*       data'name : string R.t > listing R.t *)
 
 
 (*   (\** {5 Misc} *\) *)
