0d5707148627f0de6233f3a05b91f6e0b0a88dc3
Philippe Veber <philippe.veber@gmail.com>
2014-09-17 05:33:58 +0200

    graphics: restaured hist

--- src/graphics/r_graphics.ml
+++ src/graphics/r_graphics.ml
@@ -1,55 +1,41 @@
 let () = ignore (R.eval_string "require(graphics, quietly=TRUE)")
 
-(* module Stub = struct *)
+module Symbol = struct
 
-(*   let hist = R.symbol "hist" *)
+  let hist = R.symbol "hist"
 
-(* end *)
+end
 
-(* type hist = < breaks : float list R.t ; *)
-(*               counts : float list R.t ; *)
-(*               density : float list R.t ; *)
-(*               mids : float list R.t ; *)
-(*               xname : string R.t ; *)
-(*               equidist : bool R.t > *)
+class hist o = object
+  method breaks = R.floats_of_t (o ## breaks)
+  method counts = R.floats_of_t (o ## counts)
+  method density = R.floats_of_t (o ## density)
+  method mids = R.floats_of_t (o ## mids)
+  method xname = R.string_of_t (o ## xname)
+  method equidist = R.bool_of_t (o ## equidist)
+end
 
-(* let r_breaks = function *)
-(* | `n n -> Obj.magic (R.int n) *)
-(* | `l v -> Obj.magic v *)
-(* | `m `Sturges -> Obj.magic (R.string "Sturges") *)
-(* | `m `Scott -> Obj.magic (R.string "Scott") *)
-(* | `m `FD -> Obj.magic (R.string "FD") *)
+let any x = (x : _ #R.ty R.t :> < > R.t)
 
-(* let hist ?breaks ?freq ?include_lowest ?right ?main ?xlab ?ylab ?xlim ?ylim x =  *)
-(*   R.eval Stub.hist [ *)
-(*     R.arg (fun x -> x)                  x ; *)
-(*     R.opt r_breaks     "breaks"         breaks ; *)
-(*     R.opt R.bool       "freq"           freq ; *)
-(*     R.opt R.bool       "include_lowest" include_lowest ; *)
-(*     R.opt R.bool       "right"          right ; *)
-(*     R.opt R.string     "main"           main; *)
-(*     R.opt R.string     "xlab"           xlab ; *)
-(*     R.opt R.string     "ylab"           ylab ; *)
-(*     R.opt R.float      "xlim"           xlim ; *)
-(*     R.opt R.float      "ylim"           ylim ; *)
-(*   ] *)
+let r_breaks = function
+| `n n -> any (R.int n)
+| `l v -> any (R.floats v)
+| `m `Sturges -> any (R.string "Sturges")
+| `m `Scott -> any (R.string "Scott")
+| `m `FD -> any (R.string "FD")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let hist ?breaks ?freq ?include_lowest ?right ?main ?xlab ?ylab ?xlim ?ylim ?plot x =
+  R.eval Symbol.hist [
+    R.arg R.floats                  x ;
+    R.opt r_breaks     "breaks"         breaks ;
+    R.opt R.bool       "freq"           freq ;
+    R.opt R.bool       "include_lowest" include_lowest ;
+    R.opt R.bool       "right"          right ;
+    R.opt R.string     "main"           main;
+    R.opt R.string     "xlab"           xlab ;
+    R.opt R.string     "ylab"           ylab ;
+    R.opt R.float      "xlim"           xlim ;
+    R.opt R.float      "ylim"           ylim ;
+    R.opt R.bool       "plot"           plot ;
+  ]
+  |> new hist
