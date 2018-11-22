469d0514bc703469f81cbeaafd90ec8d8111a49b
Philippe Veber <philippe.veber@gmail.com>
2018-11-22 01:03:27 +0100

    examples: fixed test_suite

--- examples/test_suite.ml
+++ examples/test_suite.ml
@@ -11,14 +11,14 @@ let _ =
   try
     ignore (R.eval_string "azer")
   with
-    R.Runtime_error (x,y) -> print_string y
+    R.Runtime_error (_, y) -> print_string y
 ;;
 
 let _ =
   try
     ignore (R.eval_string "list()[[1]]")
   with
-    R.Runtime_error (x,y) -> print_endline y
+    R.Runtime_error (_, y) -> print_endline y
 ;;
 
 let _ =
