65ea86bf41c35fc303a937ccbf53c404755965ca
Philippe Veber <philippe.veber@gmail.com>
2020-09-29 11:59:14 +0200

    utils: added read'table

--- lib/utils/OCamlR_utils.ml
+++ lib/utils/OCamlR_utils.ml
@@ -11,6 +11,36 @@ let data ?envir name =
   ]
   |> ignore
 
+let r_numerals o =
+  R.string (
+    match o with
+    | `allow'loss -> "allow.loss"
+    | `warn'loss -> "warn.loss"
+    | `no'loss -> "no.loss"
+  )
+
+let read'table
+    ?header ?sep ?quote ?dec
+    ?numerals ?row'names ?col'names
+    ?na'strings ?check'names ?strip'white
+    ?comment'char ?stringsAsFactors file =
+  Stubs.read'table
+    ~file:(file |> R.string)
+    ?header:(header |?> R.bool)
+    ?sep:(sep |?> R.string)
+    ?quote:(quote |?> R.string)
+    ?dec:(dec |?> R.string)
+    ?numerals:(numerals |?> r_numerals)
+    ?col'names:(col'names |?> R.bool)
+    ?row'names:(row'names |?> R.bool)
+    ?na'strings:(na'strings |?> R.string)
+    ?check'names:(check'names |?> R.bool)
+    ?strip'white:(strip'white |?> R.bool)
+    ?comment'char:(comment'char |?> R.string)
+    ?stringsAsFactors:(stringsAsFactors |?> R.bool)
+    ()
+  |> Dataframe.Unsafe.of_r
+
 let write'table ?file ?sep ?col'names ?row'names ?quote x =
   Stubs.write'table
     ?file:(file |?> R.string)
--- lib/utils/OCamlR_utils.mli
+++ lib/utils/OCamlR_utils.mli
@@ -4,6 +4,22 @@ val data :
   ?envir:Environment.t ->
   string -> unit
 
+val read'table :
+  ?header:bool ->
+  ?sep:string ->
+  ?quote:string ->
+  ?dec:string ->
+  ?numerals:[`allow'loss | `warn'loss | `no'loss] ->
+  ?row'names:bool ->
+  ?col'names:bool ->
+  ?na'strings:string ->
+  ?check'names:bool ->
+  ?strip'white:bool ->
+  ?comment'char:string ->
+  ?stringsAsFactors:bool ->
+  string ->
+  Dataframe.t
+
 val write'table :
   ?file:string ->
   ?sep:string ->
