5ac220948024274aa525e24312f617ffa052dcbe
Philippe Veber <philippe.veber@gmail.com>
2020-10-02 20:16:58 +0200

    R: reorder modules in interface

--- lib/r.mli
+++ lib/r.mli
@@ -53,58 +53,6 @@
 *)
 
 
-(** {2 Declaration of environment - Initialisation.} *)
-
-module type Environment = sig
-  (**  [Environment] is the type of a module containing all necessary
-       informations and data in order to set up the R interpreter
-       properly. *)
-
-  val name : string
-  (**  This is the [name] of the first argument of [argv] for R.
-       Mandatory, otherwise [libR.so] segfaults immediately. *)
-
-  val options : string list
-  (**  Other command line options passed to the [libR.so] library when
-       initialising R.
-
-       @see "R reference manual" File refman.pdf, page 452, section intitled
-       'Startup - Initialization at Start of an R Session' for details
-       about the most important command line options.
-       @see <http://cran.r-project.org/doc/manuals/R-intro.html#Invoking-R>
-       For command line options.
-  *)
-
-  val signal_handlers : bool
-  (**  If set to [false], asks R not to install its signal handlers. I've
-       been experiencing weird issues with R signal handlers, since, for
-       instance, a [SIGSEGV] is sometimes caught by [libR.so], and R then
-       asks whether or not you want to save your workspace, et ceteræ... By
-       default, set to false.
-  *)
-
-  val env : (string * string) list
-  (**  These are environment variables that needs to be set before
-       initialising the R interpreter. In the [Standard] module, these
-       values are determined when the binding itself is compiled.
-  *)
-
-  val packages : string list option
-  (**  Packages loaded on startup. If set to [None], load the usual standard
-       library. Otherwise, if set to [Some p], load packages [p] in place of
-       the standard library. In the [Standard] module, this is set to [Some []]. *)
-
-end
-
-
-(** {2 Standard environment - Initialisation.} *)
-
-module Standard : Environment
-(**  The [Standard] module contains initialisation details for libR.so.
-     These informations are determined when the binding is being compiled.
-*)
-
-
 (** {2 Internal representation of R values.} *)
 
 type sexp
@@ -509,6 +457,52 @@ val terminate : unit -> unit
 
 module type Interpreter = sig end
 (**  Module type of an R interpreter. *)
+
+module type Environment = sig
+  (**  [Environment] is the type of a module containing all necessary
+       informations and data in order to set up the R interpreter
+       properly. *)
+
+  val name : string
+  (**  This is the [name] of the first argument of [argv] for R.
+       Mandatory, otherwise [libR.so] segfaults immediately. *)
+
+  val options : string list
+  (**  Other command line options passed to the [libR.so] library when
+       initialising R.
+
+       @see "R reference manual" File refman.pdf, page 452, section intitled
+       'Startup - Initialization at Start of an R Session' for details
+       about the most important command line options.
+       @see <http://cran.r-project.org/doc/manuals/R-intro.html#Invoking-R>
+       For command line options.
+  *)
+
+  val signal_handlers : bool
+  (**  If set to [false], asks R not to install its signal handlers. I've
+       been experiencing weird issues with R signal handlers, since, for
+       instance, a [SIGSEGV] is sometimes caught by [libR.so], and R then
+       asks whether or not you want to save your workspace, et ceteræ... By
+       default, set to false.
+  *)
+
+  val env : (string * string) list
+  (**  These are environment variables that needs to be set before
+       initialising the R interpreter. In the [Standard] module, these
+       values are determined when the binding itself is compiled.
+  *)
+
+  val packages : string list option
+  (**  Packages loaded on startup. If set to [None], load the usual standard
+       library. Otherwise, if set to [Some p], load packages [p] in place of
+       the standard library. In the [Standard] module, this is set to [Some []]. *)
+
+end
+
+module Standard : Environment
+(**  The [Standard] module contains initialisation details for libR.so.
+     These informations are determined when the binding is being compiled.
+*)
 
 module Interpreter (Env : Environment) : Interpreter
 (**  Functor used to initialise statically an R interpreter, given initialisation
