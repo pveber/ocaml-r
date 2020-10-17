4b170859877f395de96b303279daf3c4ca205a9b
Jason Nielsen <drjdnielsen@gmail.com>
2020-10-17 11:47:12 -0400

    Fix pkg-config and clean up Configurator

--- lib/config/discover.ml
+++ lib/config/discover.ml
@@ -1,21 +1,16 @@
-open Base
-open Stdio
 module C = Configurator.V1
 
-let write_sexp fn sexp =
-  Out_channel.write_all fn ~data:(Sexp.to_string sexp)
+let cfig  c =
+  let default = { C.Pkg_config.libs = ["-lR"]; cflags = [] } in
+  let conf =
+    match C.Pkg_config.get c with
+    | None -> default
+    | Some pc ->
+       match C.Pkg_config.query pc ~package:"libR libRmath" with
+       | None -> default
+       | Some deps -> deps in
 
-let () =
-  C.main ~name:"OCaml_R" (fun c ->
-      let default : C.Pkg_config.package_conf =
-        { libs   = ["-lR"] ; cflags = [] }
-      in
-      let conf =
-        match C.Pkg_config.get c with
-        | None -> default
-        | Some pc ->
-          Option.value (C.Pkg_config.query pc ~package:"libR libRmath") ~default
-      in
-      write_sexp "c_flags.sexp"         (sexp_of_string (String.concat ~sep:" " conf.cflags)) ;
-      write_sexp "c_library_flags.sexp" (sexp_of_string (String.concat ~sep:" " conf.libs))
-    )
+  C.Flags.write_sexp "c_flags.sexp" conf.cflags;
+  C.Flags.write_sexp "c_library_flags.sexp" conf.libs;;
+
+C.main ~name:"OCaml-R" cfig;;
