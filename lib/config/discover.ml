229b0215b754344e0d81d14acd131baafc36f923
Jason Nielsen <drjdnielsen@gmail.com>
2020-10-22 09:08:42 -0400

    Clean up Configurator file

--- lib/config/discover.ml
+++ lib/config/discover.ml
@@ -1,25 +1,26 @@
 module C = Configurator.V1
 module S = String
 
+let trim_to_list str =
+  S.split_on_char ' ' (S.trim str)
+
 let cfig c =
   let default: C.Pkg_config.package_conf = {
       libs = ["-lR"; "-lRmath"];
       cflags = [] }
   in
-  let rlib = C.Process.run c "pkg-config" ["--libs-only-L"; "--libs-only-l"; "libR"] in
-  let rmlib = C.Process.run c "pkg-config" ["--libs-only-L"; "--libs-only-l"; "libRmath"] in
-  let rflg = C.Process.run c "pkg-config" ["--cflags"; "libR"] in
-  let rmflg = C.Process.run c "pkg-config" ["--cflags"; "libRmath"] in
-  let chk = (rlib.exit_code, rmlib.exit_code, rflg.exit_code, rmflg.exit_code) in
-  let lib = (S.split_on_char ' ' (S.trim rlib.stdout)) @ (S.split_on_char ' ' (S.trim rmlib.stdout)) in
-  let flg = (S.split_on_char ' ' (S.trim rflg.stdout)) @ (S.split_on_char ' ' (S.trim rmflg.stdout)) in  
+  let rlib = C.Process.run c "pkg-config" ["--libs-only-L"; "--libs-only-l"; "libR"; "libRmath"] in
+  let rflg = C.Process.run c "pkg-config" ["--cflags"; "libR"; "libRmath"] in
+  let chk = (rlib.exit_code, rflg.exit_code) in
+  let lib = trim_to_list rlib.stdout in
+  let flg = trim_to_list rflg.stdout in  
   let discover: C.Pkg_config.package_conf = {
       libs = lib;
       cflags = flg }
   in
   let conf =
     match chk with
-    | (0, 0, 0, 0) -> discover
+    | (0, 0) -> discover
     | _ -> default
   in
   C.Flags.write_sexp "c_flags.sexp" conf.cflags;
